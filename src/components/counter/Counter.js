17279d483d6aadabdaacb1f312438eb727f4cc3e
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-11-17 22:00:06 +0200

    add component Counter

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,11 @@
+
+const Counter = () => {
+    return <div>
+        <button>Start/Stop</button>
+        <button>Wait</button>
+        <button>Reset</button>
+    </div>
+
+}
+
+export default Counter;
